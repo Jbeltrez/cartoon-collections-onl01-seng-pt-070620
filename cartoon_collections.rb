efa80d34d3898bad27d8642747da3913b44d3432
john beltrez <beltrezjohn@gmail.com>
2020-07-17 10:57:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def find_the_cheese(array)# code an argument here
   
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.detect do |cheese| 
-  if cheese_types.include?(cheese)}
+  cheese_types.include?(cheese)}
     puts "#{cheese}"
   else 
     nil 
