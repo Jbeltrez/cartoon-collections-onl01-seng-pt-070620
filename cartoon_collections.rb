08c833fa0f27fcf7e0d84ebbc51868cdcd42b9ab
john beltrez <beltrezjohn@gmail.com>
2020-07-17 10:15:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf_name, index|
+    puts "#{index + 1} #{dwarf_name}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|name| name.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
