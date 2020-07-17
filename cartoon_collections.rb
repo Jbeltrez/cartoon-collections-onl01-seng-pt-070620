7d66c13f1d5711c4c5ab42487231830727ada656
john beltrez <beltrezjohn@gmail.com>
2020-07-17 10:20:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.keep_if# code an argument here
+  array.keep_if {|call| call.length > 4}# code an argument here
   # Your code here
 end
 
