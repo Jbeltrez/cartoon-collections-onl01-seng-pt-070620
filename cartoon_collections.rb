3753fb1e59cfaa3c97a83f33f027e9153a38e53c
john beltrez <beltrezjohn@gmail.com>
2020-07-17 10:18:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ def summon_captain_planet(array)
   array.map {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  array.# code an argument here
   # Your code here
 end
 
